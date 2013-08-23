013e14420c0665fa68a007058067954427429def
Andrei Senchuk <taviscaron@yandex.ru>
2013-08-23 17:20:21 +0300

    aircraft info fragment instantiating from xml

--- android/Airliners/src/net/taviscaron/airliners/activities/AircraftInfoActivity.java
+++ android/Airliners/src/net/taviscaron/airliners/activities/AircraftInfoActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import net.taviscaron.airliners.R;
 import net.taviscaron.airliners.fragments.AircraftInfoFragment;
 
 import java.util.regex.Matcher;
@@ -17,46 +18,33 @@ import java.util.regex.Pattern;
 public class AircraftInfoActivity extends SherlockFragmentActivity {
     public static final String AIRCRAFT_INFO_ACTION = "net.taviscaron.airliners.AIRCRAFT_INFO";
     public static final String AIRCRAFT_ID_KEY = "aircraftId";
-    private static final String AIRCRAFT_INFO_FRAGMENT_TAG = "aircraftInfoFragment";
-
-    private AircraftInfoFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.aircraft_info_fragment);
 
-        FragmentManager fm = getSupportFragmentManager();
-        fragment = (AircraftInfoFragment)fm.findFragmentByTag(AIRCRAFT_INFO_FRAGMENT_TAG);
-        if(fragment == null) {
-            fragment = new AircraftInfoFragment();
-            fm.beginTransaction().add(android.R.id.content, fragment, AIRCRAFT_INFO_FRAGMENT_TAG).commit();
-        } else {
-            // fragment is already created - just restore state
-            setIntent(null);
-        }
-    }
+        // state is not restoring
+        if(savedInstanceState == null) {
+            AircraftInfoFragment fragment = (AircraftInfoFragment)getSupportFragmentManager().findFragmentById(R.id.aircraft_info_fragment);
+            String id = null;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        String id = null;
-
-        Intent intent = getIntent();
-        if(intent != null) {
-            if(AIRCRAFT_INFO_ACTION.equals(intent.getAction())) {
-                id = intent.getStringExtra(AIRCRAFT_ID_KEY);
-            } else if(Intent.ACTION_VIEW.equals(intent.getAction())) {
-                Uri uri = intent.getData();
-                Matcher matcher = Pattern.compile("/photo(/.*?)*?/(\\d+)").matcher(uri.getPath());
-                if(matcher.find()) {
-                    id = matcher.group(2);
+            Intent intent = getIntent();
+            if(intent != null) {
+                if(AIRCRAFT_INFO_ACTION.equals(intent.getAction())) {
+                    id = intent.getStringExtra(AIRCRAFT_ID_KEY);
+                } else if(Intent.ACTION_VIEW.equals(intent.getAction())) {
+                    Uri uri = intent.getData();
+                    Matcher matcher = Pattern.compile("/photo(/.*?)*?/(\\d+)").matcher(uri.getPath());
+                    if(matcher.find()) {
+                        id = matcher.group(2);
+                    }
                 }
             }
-        }
 
-        if(id != null) {
-            fragment.loadAircraftInfo(id);
+            if(id != null) {
+                fragment.loadAircraftInfo(id);
+            }
         }
     }
 }
